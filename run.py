62f4940bede9467018d167d4650fc25ebe44806c
chibimiku <dreamcosine@gmail.com>
2018-05-02 20:15:28 +0800

    update for bcy fetching
    oh nice.

--- run.py
+++ run.py
@@ -7,24 +7,20 @@ import time
 import imghdr
 import json
 
-#从cookie.txt里读取cookie
-#这个可以从chrome浏览器f12，然后在console里打document.cookie来显示当前cookie
-#注意cookie不是一直有效的，每次跑之前手动更新吧
-with open("cookie.txt", 'r', encoding="utf8") as fp:
-    cookie = fp.readlines()
+#有效的站点类型
+good_type = ['weibo', 'bcy']
 
-saving_path = "d:/images/weibo/"
-fetch_interval = 2 #抓取间隔多少秒
-headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 7.0; HUAWEI NXT-AL10 Build/HUAWEINXT-AL10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 MQQBrowser/6.2 TBS/043804 Mobile Safari/537.36'}
-headers["Content-Type"] = 'utf-8'  #编码设置，这个不用动
-
-#urllist = ['https://m.weibo.cn/status/FBgBzvIlu']
-urllist = []
-with open('fetch_list.txt', 'r' , encoding="utf-8") as fp:
-    for line in fp:
-        line = line.strip()
-        if(len(line) > 0):
-            urllist.append(line)
+def init_cookie(site_name):
+    #从cookie.txt里读取cookie
+    #这个可以从chrome浏览器f12，然后在console里打document.cookie来显示当前cookie
+    #注意cookie不是一直有效的，每次跑之前手动更新吧
+    cookie=''
+    with open("data/" + site_name + "_cookies.txt", 'r', encoding="utf8") as fp:
+        cookie = fp.readlines()
+    cookie = str(cookie)
+    print ("Loaded cookies:" + cookie)
+    #headers["Cookie"] = cookie
+    return cookie
 
 def find_betweens(s, first, last):
     ret_list = []
@@ -90,8 +86,21 @@ def unicode_convert(in_str):
         
 def get_datas_from_status(html):
     pass
+
+#万能入口，解析url并选择合适的load方法.    
+def load_page(page, saving_path, common_headers, real_download_image = True):
+    if('weibo.com' in page):
+        return load_weibo_page(page, saving_path + 'weibo/', common_headers)
+    elif('weibo.cn' in page):
+        return load_weibo_page(page, saving_path + 'weibo/', common_headers)
+    elif('bcy.net' in page):
+        return load_bcy_page(page, saving_path + 'bcy/', common_headers)
+    else:
+        print ("[WARNING]cannot found entry for url:" + page)
         
-def load_page(page):
+def load_weibo_page(page, saving_path, my_headers, need_save_image = True):
+    my_cookie = init_cookie('weibo')
+    headers["Cookie"] = my_cookie
     try:
         print ("try to get url:" + page + " ...")
         write_log("[INFO]try to fetch..." + page)
@@ -136,25 +145,73 @@ def load_page(page):
         print (pics)
         front_url = 'http://' + original_pic.split("http://")[1].split("/")[0] + '/large/'
 
+        
         #拼接url
         if(len(pics) > 0):
             for s_url in pics:
-                download_image(front_url + s_url , headers, final_save_dir, user_name + "_")
-                time.sleep(fetch_interval)
+                if (need_save_image):
+                    download_image(front_url + s_url , headers, final_save_dir, user_name + "_")
+                    time.sleep(fetch_interval)
+                else:
+                    print ('[img]' + front_url + s_url + '.jpg[/img]')
+                
         else:
-            download_image(original_pic, headers, final_save_dir, user_name + "_")
+            if (need_save_image):
+                download_image(original_pic, headers, final_save_dir, user_name + "_")
+            else:
+                print ('[img]' + original_pic + '.jpg[/img]')
         return True
     except Exception as e:
         print (e)
         write_log("failed to load page:" + str(page))
         return False
 
+def load_bcy_page(page, saving_path, my_headers):
+    my_cookie = init_cookie('bcy')
+    my_headers["Cookie"] = my_cookie
+    try:
+        r = requests.get(page, data={}, timeout = 15, headers=my_headers)
+        html = r.text
+        output_file(html, 'data/debug.html')
+        author_tmp = find_between(html, '<a class="_avatar', '>')
+        author_name = find_between(author_tmp, 'title="', '"')
+        author_uid = find_between(author_tmp, 'href="/u/', '"')
+        title = find_between(html, '<h1 class="js-post-title">', '</h1>').strip()
+        img_lines = find_betweens(html, "<img class='detail_std detail_clickable' src='", "' />")
+        print ("found " + str(len(img_lines)) + " pic(s) in " + page + " ...")
+        final_save_dir = saving_path + author_name #保存位置
+        #用mkdir一下新建目录，省得写入失败
+        print ("use " + final_save_dir + " as target dir...")
+        create_rs = new_mkdirs(final_save_dir)
+        for img_line in img_lines:
+            if('/w650' in img_line):
+                img_line = img_line.replace('/w650', '')
+            download_image(img_line, my_headers, final_save_dir, author_name + "_" + title + "_")
+            time.sleep(fetch_interval)
+    except Exception as e:
+        print (e)
+        write_log("failed to load page:" + str(page))
+        return False
+    return True
+
+saving_path = "d:/images/"
+fetch_interval = 2 #抓取间隔多少秒
+headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 7.0; HUAWEI NXT-AL10 Build/HUAWEINXT-AL10; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 MQQBrowser/6.2 TBS/043804 Mobile Safari/537.36'}
+headers["Content-Type"] = 'utf-8'  #编码设置，这个不用动
+urllist = []
+with open('fetch_list.txt', 'r' , encoding="utf-8") as fp:
+    for line in fp:
+        line = line.strip()
+        if(len(line) > 0):
+            urllist.append(line)
+
 count = 0
 fail_count = 0
+
 for page in urllist:
-    rs = load_page(page)
+    rs = load_page(page, saving_path, headers, True)
     if(not rs):
         fail_count = fail_count + 1
     count = count + 1
-print ("task done. run for " + str(count) + " page(s). " + str(fail_count) + " page(s) failed. check error log.")
 
+print ("task done. run for " + str(count) + " page(s). " + str(fail_count) + " page(s) failed. check error log.")
